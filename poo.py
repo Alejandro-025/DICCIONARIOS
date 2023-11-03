0610ed5438406420296a3b210e74083725e09e04
Alejandro <mateo.alejandro020825@gmail.com>
2023-11-03 18:15:43 -0500

    commit

---
+++ poo.py
@@ -0,0 +1,100 @@
+
+#Crear una lista de diccionarios con la siguiente estructura:
+
+clasepoo = {{"Nombre": "Alejandro",
+             "Apellido": "Garcia",
+             "Promedio": 3.78,
+             "Nivel de POO": 2,
+             "Edad": 21,
+             "Estado Civil": "Soltera"
+}, 
+             {"Nombre": "Juan",
+             "Apellido": "Rivera",
+             "Promedio": 3.4,
+             "Nivel de POO": 3,
+             "Edad": 21,
+             "Estado Civil": "Soltera"
+}, 
+             {"Nombre": "Sandra",
+             "Apellido": "Junco",
+             "Promedio": 4.39,
+             "Nivel de POO": 2,
+             "Edad": 23,
+             "Estado Civil": "Soltera"
+},  
+             {"Nombre": "Brayan",
+             "Apellido": "Ortiz",
+             "Promedio": 3.09,
+             "Nivel de POO": 2,
+             "Edad": 20,
+             "Estado Civil": "Soltera"
+},
+             {"Nombre": "Jhon",
+             "Apellido": "Neva",
+             "Promedio": 3.50,
+             "Nivel de POO": 2,
+             "Edad": 20,
+             "Estado Civil": "Soltera"
+},
+             {"Nombre": "Luisa",
+             "Apellido": "Betancourt",
+             "Promedio": 3.50,
+             "Nivel de POO": 2,
+             "Edad": 19,
+             "Estado Civil": "Soltera"
+}, 
+             {"Nombre": "Linda",
+             "Apellido": "Ramos",
+             "Promedio": 3.61,
+             "Nivel de POO": 2,
+             "Edad": 25,
+             "Estado Civil": "Con novio"
+},
+             {"Nombre": "Maria",
+             "Apellido": "Pardo",
+             "Promedio": 3.80,
+             "Nivel de POO": 2,
+             "Edad": 23,
+             "Estado Civil": "Con novio"
+},
+             {"Nombre": "Diana",
+             "Apellido": "Soto",
+             "Promedio": 3.60,
+             "Nivel de POO": 2,
+             "Edad": 20,
+             "Estado Civil": "Con novio"
+},  
+             {"Nombre": "Johan",
+             "Apellido": "Galindo",
+             "Promedio": 3.38,
+             "Nivel de POO": 2,
+             "Edad": 22,
+             "Estado Civil": "Soltera"
+},  
+             {"Nombre": "Angel",
+             "Apellido": "Molano",
+             "Promedio": 3.60,
+             "Nivel de POO": 3,
+             "Edad": 24,
+             "Estado Civil": "Con novio"
+},  
+             {"Nombre": "Nikole",
+             "Apellido": "Florez",
+             "Promedio": 4.19,
+             "Nivel de POO": 2.5,
+             "Edad": 21,
+             "Estado Civil": "Soltera"
+},  
+             {"Nombre": "Andres",
+             "Apellido": "Segura",
+             "Promedio": 4.10,
+             "Nivel de POO": 3,
+             "Edad": 24,
+             "Estado Civil": "Casada"
+}
+             
+}  
+
+print(Clasepoo)
+
+
